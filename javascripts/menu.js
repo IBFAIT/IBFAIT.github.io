099354d229d7b350261b932945a5d880be0c1f96
IBFAIT <flavio.trolese@4quant.com>
2016-04-02 14:18:51 +0200

    grmpf

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-    $('button.hambi-topright').click(function(event) {
-        var p = $(this).position();
-        $(this).toggleClass('is-active').toggleClass('hambiFix').css({
-            top: p.top,
-            left: p.left
-        });
-        $('body').toggleClass('noscroll');
-        $('#layerNav').toggleClass('layernav-active');
-        $(document).keyup(function(e) {
-            if (e.keyCode == 27 && $('body').hasClass('noscroll')) {
-                $('button.hambi-topright').removeClass('is-active');
-                $('body').removeClass('noscroll');
-                $('#layerNav').removeClass('layernav-active');
-            }
-        });
-    });
-});
+$(document).ready(function(){$("button.menu").click(function(e){var o=$(this).position();$(this).toggleClass("is-active").toggleClass("menu-fix").css({top:o.top,left:o.left}),$("body").toggleClass("noscroll"),$("#layerNav").toggleClass("layernav-active"),$(document).keyup(function(e){27==e.keyCode&&$("body").hasClass("noscroll")&&($("button.menu").removeClass("is-active"),$("body").removeClass("noscroll"),$("#layerNav").removeClass("layernav-active"))})})});
